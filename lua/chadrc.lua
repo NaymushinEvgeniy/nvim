e9b67eb01b8e37ce7cc7e128575baf958e898e2d
EvgeniyN MacPro <evgeniy@naymushin.xyz>
2024-11-04 13:59:00 +0300

    Setup colors and fixes backspace

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,9 +4,48 @@
 ---@type ChadrcConfig
 local M = {}
 
- M.base46 = {
+M.base46 = {
   theme = "dark_horizon",
   transparency = true,
+  hl_override = {
+    Comment = { italic = true },
+    ["@Comment"] = { italic = true },
+    DiffChange = {
+     bg = "#464414",
+     fg = "none",
+    },
+    DiffAdd = {
+      bg = "#103507",
+      fg = "NONE",
+    },
+    DiffRemoved = {
+      bg = "#461414",
+      fg = "NONE",
+    },
+  }
  }
 
+M.ui = {
+ statusline = {
+    theme = "vscode_colored"
+  }
+}
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+       "                            ",
+       "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+       "   ▄▀███▄     ▄██ █████▀    ",
+       "   ██▄▀███▄   ███           ",
+       "   ███  ▀███▄ ███           ",
+       "   ███    ▀██ ███           ",
+       "   ███      ▀ ███           ",
+       "   ▀██ █████▄▀█▀▄██████▄    ",
+       "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+       "                            ",
+       "     Powered By  eovim    ",
+       "                            ",
+    },
+  }
  return M
