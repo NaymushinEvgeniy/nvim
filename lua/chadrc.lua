6079408df25400ac1904abad5f2abcd791a01d6b
EvgeniyN MacPro <evgeniy@naymushin.xyz>
2024-11-04 13:30:09 +0300

    Enable base46 plugin

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,40 +4,9 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-  theme = "kanagawa",
+ M.base46 = {
+  theme = "dark_horizon",
   transparency = true,
-  statusline = {
-    theme = "vscode_colored",
-  },
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "██████╗ ██╗   ██╗██████╗ ██████╗ ██╗     ███████╗",
-      "██╔══██╗██║   ██║██╔══██╗██╔══██╗██║     ██╔════╝",
-      "██████╔╝██║   ██║██████╔╝██████╔╝██║     █████╗  ",
-      "██╔═══╝ ██║   ██║██╔══██╗██╔═══╝ ██║     ██╔══╝  ",
-      "██║     ╚██████╔╝██║  ██║██║     ███████╗███████╗",
-      "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝╚══════╝",
-    },
-  },
+ }
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-    DiffChange = {
-      bg = "#464414",
-      fg = "none",
-    },
-    DiffAdd = {
-      bg = "#103507",
-      fg = "none",
-    },
-    DiffRemoved = {
-      bg = "#461414",
-      fg = "none",
-    },
-  },
-}
-
-return M
+ return M
